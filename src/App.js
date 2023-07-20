7838151696f36b86ce80ea45eb6a6d9d8a538088
amishari <81028814+amishari@users.noreply.github.com>
2023-07-20 07:54:45 +0330

    stats in footer completed

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ export default function App() {
     <div className="App">
       <Form onAdd={addHandler} />
       <List items={items} onDelete={deleteHandler} onChecked={toggleHandler} />
+      <Stats items={items} />
     </div>
   );
 }
@@ -78,5 +79,27 @@ function List({ items, onDelete, onChecked }) {
         ))}
       </ul>
     </div>
+  );
+}
+function Stats({ items }) {
+  if(!items.length){
+    return(
+      <p className="stats">
+        Start adding something to your list.
+      </p>
+    )
+  }
+  const itemQty = items.length;
+  const packed = items.filter((item) => item.done).length;
+  const percent = Math.round((packed / itemQty) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {percent === 100
+          ? "✅You are done.Ready to go! Have a nice trip!!"
+          : `You have ${itemQty} items on your list, and you have already ${packed} (${percent})%
+        packed.`}
+      </em>
+    </footer>
   );
 }
